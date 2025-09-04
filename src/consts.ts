df6e97a27137de29ae8a5185850a0f68c6c4d879
Sander Geraedts <sgeraedts02@anwb.nl>
2025-09-04 14:42:51 +0200

    fix: update event date and improve EventCard layout

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = 'Boeliebots';
 export const SITE_DESCRIPTION = 'Boeliebots is een combat robots-groep uit Groningen, toegewijd aan het bouwen, strijden en promoten van robotgevechten. Doe mee om samen vechtrobots te ontwerpen, bouwen en ermee te vechten!';
-export const NEXT_EVENT = '22 june 2025';
+export const NEXT_EVENT = '16 november 2025';
 export const CLOSE_DAYS_BEFORE_EVENT = 3; // Number of days before the event that the site should be closed for new registrations
 export const LIVESTREAM_URL = 'https://www.youtube.com/live/xrhdEJzg84c';
