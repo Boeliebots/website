b680f5293cd4e5125662f39a71df1f220e7a337e
Herman Kopinga <herman@kopinga.nl>
2026-03-14 22:00:49 +0100

    Update LIVESTREAM_URL to new YouTube link

--- src/consts.ts
+++ src/consts.ts
@@ -5,4 +5,4 @@ export const SITE_TITLE = 'Boeliebots';
 export const SITE_DESCRIPTION = 'Boeliebots is een combat robots-groep uit Groningen, toegewijd aan het bouwen, strijden en promoten van robotgevechten. Doe mee om samen vechtrobots te ontwerpen, bouwen en ermee te vechten!';
 export const NEXT_EVENT = '15 march 2026';
 export const CLOSE_DAYS_BEFORE_EVENT = 7; // Number of days before the event that the site should be closed for new registrations
-export const LIVESTREAM_URL = 'https://www.youtube.com/live/Th3bJ8Tgdno';
+export const LIVESTREAM_URL = 'https://www.youtube.com/live/bdyJjwGWtIE';
